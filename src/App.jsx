04486185dc7aee572f0f56b7e4792f174b34d25b
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-02-12 10:18:44 +0700

    Add Leaderboard page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
+import Leaderboard from './pages/Leaderboard';
 
 function App() {
   return (
-    <>
-      <h1>Welcome to React Vite Micro App!</h1>
-      <p>Hard to get more minimal than this React app.</p>
-    </>
+    <Leaderboard />
   );
 }
 
