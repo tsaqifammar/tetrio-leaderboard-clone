7aa003098c170cf8eabefed7f63060b1a149f5ce
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-02-12 22:00:44 +0700

    Add changeCountry functionality within Leaderboard component

--- src/components/CountrySelector.jsx
+++ src/components/CountrySelector.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import countries from '../data/country_names.json';
 
-function CountrySelector() {
+function CountrySelector({ country, onChangeHandler }) {
   return (
     <label htmlFor="country">
       Country
-      <select name="country" id="country">
+      <select value={country} onChange={(e) => onChangeHandler(e.target.value)}>
+        <option value="">All Countries</option>
         {
           Object.entries(countries).map(([code, name]) => (
             <option key={code} value={code}>{name}</option>
@@ -15,5 +17,10 @@ function CountrySelector() {
     </label>
   );
 }
+
+CountrySelector.propTypes = {
+  country: PropTypes.string.isRequired,
+  onChangeHandler: PropTypes.func.isRequired,
+};
 
 export default CountrySelector;
--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import CountrySelector from '../components/CountrySelector';
 import LeaderboardTable from '../components/LeaderboardTable';
 
 function Leaderboard() {
+  const [country, setCountry] = useState('');
+  const [players, setPlayers] = useState([]);
+
+  const changeCountry = async (c) => {
+    setCountry(c);
+    setPlayers(await axios.get('http://localhost:4000/players', { params: { country: c } })
+      .then((res) => res.data));
+  };
+
   return (
     <div>
       <h1>Tetr.io Leaderboard</h1>
-      <CountrySelector />
+      <CountrySelector country={country} onChangeHandler={changeCountry} />
       <hr />
       <LeaderboardTable />
     </div>
