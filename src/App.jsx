91757a38c1a66c6c19f1cfee8f5e7bc6cd98e51a
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-02-12 10:13:33 +0700

    Fix code styling for initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React from 'react';
 
-export default () => (
-  <>
-    <h1>Welcome to React Vite Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+function App() {
+  return (
+    <>
+      <h1>Welcome to React Vite Micro App!</h1>
+      <p>Hard to get more minimal than this React app.</p>
+    </>
+  );
+}
+
+export default App;
