a4cb13d1a7a95b2ab9a504ccd855a9536f4145e0
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-02-13 09:17:12 +0700

    Add fetch player data on mount

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -1,24 +1,39 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CountrySelector from '../components/CountrySelector';
 import LeaderboardTable from '../components/LeaderboardTable';
 
 function Leaderboard() {
+  const [isLoading, setIsLoading] = useState(true);
   const [country, setCountry] = useState('');
   const [players, setPlayers] = useState([]);
 
+  useEffect(async () => {
+    const url = 'http://localhost:4000/players';
+    setPlayers(await axios.get(url).then((res) => res.data));
+    setIsLoading(false);
+  }, []);
+
   const changeCountry = async (c) => {
     setCountry(c);
-    setPlayers(await axios.get('http://localhost:4000/players', { params: { country: c } })
+
+    const url = 'http://localhost:4000/players';
+    setPlayers(await axios.get(url, { params: { country: c } })
       .then((res) => res.data));
   };
 
   return (
     <div>
-      <h1>Tetr.io Leaderboard</h1>
-      <CountrySelector country={country} onChangeHandler={changeCountry} />
-      <hr />
-      <LeaderboardTable />
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <>
+          <h1>Tetr.io Leaderboard</h1>
+          <CountrySelector country={country} onChangeHandler={changeCountry} />
+          <hr />
+          <LeaderboardTable />
+        </>
+      )}
     </div>
   );
 }
